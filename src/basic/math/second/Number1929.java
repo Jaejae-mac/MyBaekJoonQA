13a7128065d3e11af9538a8be403c9bf7c383f59
jj <youngje3431@gmail.com>
2021-12-30 01:36:22 +0900

    백준 1929번 해결

--- src/basic/math/second/Number1929.java
+++ src/basic/math/second/Number1929.java
@@ -11,26 +11,21 @@ public class Number1929 {
 		String[] str = bf.readLine().split(" ");
 		int m = Integer.parseInt(str[0]);
 		int n = Integer.parseInt(str[1]);
-		int[] num = new int[n - m + 1];
-		int idx = 0;
-		for (int i = m; i <= n; i++) {
-			num[idx] = i;
-			idx++;
-		}
-		idx = 0;
+		int[] num = new int[n+1];
 		for (int i = 2; i <= n; i++) {
+			num[i] = i;
+		}
+		
+		for (int i = 2; i <= n; i++) {
+			if(num[i] == 0) continue;
 			for (int j = 2 * i; j <= n; j += i) {
-				for (int h = 0; h < num.length; h++) {
-					if(num[h] == 0) continue;
-					if (num[h] == j) {
-						num[h] = 0;
-					}
+				if(num[j] % i == 0) {
+					num[j] = 0;
 				}
-
 			}
 		}
 
-		for (int i = 0; i < num.length; i++) {
+		for (int i = m; i < num.length; i++) {
 			if (num[i] != 0)
 				System.out.println(num[i]);
 		}
